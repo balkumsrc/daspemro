88975dbbcbee095fd7a94c16dfbfc4af1ac251b6
balkumsrc <147388860+balkumsrc@users.noreply.github.com>
2026-03-13 15:01:21 +0700

    Add files via upload

---
+++ Latihan_1.1_Tipe_data_dan_variabel.py
@@ -0,0 +1,14 @@
+nama = "pensil"
+panjang = 0.7
+berat = 12
+use = True
+
+print ("saya punya",nama)
+print ("panjangnya",panjang,"mm")
+print("beratnya",berat,"gram")
+print("apakah pensil ini kamu gunakan?",use)
+
+print (type(nama))
+print (type(panjang))
+print (type(berat))
+print (type(use))
